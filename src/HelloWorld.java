45590625acb89e530fdd5c7f7c63955f1c45a582
Deepak Arjariya <deepak.arjariya@aubank.in>
2022-09-06 12:23:20 +0530

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {	
+		
+		System.out.println("Welcome to Java! Your first program in Java");
+		
+		int numberOfStudents = 32;
+		long totalKiloBytesUsed = 30_026_556_384_32_045_859L;
+		float temperature = 3945.6567878F;
+		boolean isReady = false;
+		double decimal = 2.789904589494885099;
+		System.out.println(numberOfStudents);
+		System.out.println(totalKiloBytesUsed);
+		System.out.println(temperature);
+		System.out.println(isReady);
+		System.out.println(decimal);
+		
+	}
+}
